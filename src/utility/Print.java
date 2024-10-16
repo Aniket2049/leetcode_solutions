19ead6023b39fd06a89f1c55b842f44e20f1aee8
Aniket Singh <76955108+Aniket2049@users.noreply.github.com>
2024-10-16 22:57:08 +0530

    add 49 group anagrams

--- src/utility/Print.java
+++ src/utility/Print.java
@@ -1,10 +1,23 @@
 package utility;
 
+import java.util.List;
+
 public final class Print {
-	public static void print2DArray(int[][] arr) {
+	public static void printInt2DArray(int[][] arr) {
 		for (int i = 0; i < arr.length; i++) {
 			for (int j = 0; j < arr[i].length; j++) {
 				System.out.print(arr[i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
+
+	public static void printStringListOfLists(List<List<String>> listOfList) {
+		for (int i = 0; i < listOfList.size(); i++) {
+			List<String> currentString = listOfList.get(i);
+
+			for (int j = 0; j < currentString.size(); j++) {
+				System.out.print(currentString.get(j) + " ");
 			}
 			System.out.println();
 		}
