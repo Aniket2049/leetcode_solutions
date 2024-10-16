c72380737b291d2e4d24d85fb34b1e6301ee921a
Aniket Singh <76955108+Aniket2049@users.noreply.github.com>
2024-10-16 22:11:37 +0530

    add 1 two sum

---
+++ src/neetcode/arrays/_1_TwoSum.java
@@ -0,0 +1,37 @@
+package neetcode.arrays;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+// https://leetcode.com/problems/two-sum/description/
+// https://www.youtube.com/watch?v=KLlXCFG5TnA
+public class _1_TwoSum {
+
+	public int[] twoSum(int[] nums, int target) {
+		HashMap<Integer, Integer> prevMap = new HashMap<Integer, Integer>();
+		for (int i = 0; i < nums.length; i++) {
+			int num = nums[i];
+			int diff = target - num;
+
+			if (prevMap.containsKey(diff)) {
+				return new int[] { prevMap.get(diff), i };
+			}
+
+			prevMap.put(num, i);
+		}
+
+		return new int[] {};
+	}
+
+	public static void main(String[] args) {
+//		Input: nums = [2,7,11,15], target = 9
+//		Output: [0,1]
+
+		int[] nums = { 2, 7, 11, 15 };
+		int target = 9;
+		_1_TwoSum obj = new _1_TwoSum();
+		System.out.println(Arrays.toString(obj.twoSum(nums, target)));
+
+	}
+
+}
