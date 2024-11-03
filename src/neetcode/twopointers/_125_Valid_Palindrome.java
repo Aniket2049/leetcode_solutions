d8b8d5e6e5d934ebcc72d16601a0a04b3743db9f
Aniket Singh <76955108+Aniket2049@users.noreply.github.com>
2024-11-03 16:32:53 +0530

    add another solution for 125 valid palindrome

--- src/neetcode/twopointers/_125_Valid_Palindrome.java
+++ src/neetcode/twopointers/_125_Valid_Palindrome.java
@@ -1,19 +1,19 @@
 package neetcode.twopointers;
 
 // https://leetcode.com/problems/valid-palindrome/
-
+// https://www.youtube.com/watch?v=jJXJ16kPFWg
 public class _125_Valid_Palindrome {
 
 	public boolean isPalindrome(String s) {
-		if(s.equals(" "))
+		if (s.equals(" "))
 			return true;
-		
+
 		String alphaNumericStr = s.replaceAll("[^A-Za-z0-9]", "");
 		alphaNumericStr = alphaNumericStr.toLowerCase();
 
-		if(alphaNumericStr.equals(" ") || alphaNumericStr.equals(""))
+		if (alphaNumericStr.equals(" ") || alphaNumericStr.equals(""))
 			return true;
-		
+
 		int i = 0;
 		int j = alphaNumericStr.length() - 1;
 
@@ -29,6 +29,16 @@ public class _125_Valid_Palindrome {
 		return true;
 	}
 
+	public boolean isPalindrome2(String s) {
+		StringBuilder newStr = new StringBuilder();
+		for (char c : s.toCharArray()) {
+			if (Character.isLetterOrDigit(c)) {
+				newStr.append(Character.toLowerCase(c));
+			}
+		}
+		return newStr.toString().equals(newStr.reverse().toString());
+	}
+
 	public static void main(String[] args) {
 //		Example 1:
 //		Input: s = "A man, a plan, a canal: Panama"
@@ -36,15 +46,15 @@ public class _125_Valid_Palindrome {
 //		Explanation: "amanaplanacanalpanama" is a palindrome.
 
 		String s = "A man, a plan, a canal: Panama";
-		System.out.println(new _125_Valid_Palindrome().isPalindrome(s));
-		
+		System.out.println(new _125_Valid_Palindrome().isPalindrome2(s));
+
 //		Example 2:
 //		Input: s = "race a car"
 //		Output: false
 //		Explanation: "raceacar" is not a palindrome.
-		
+
 		s = "race a car";
-		System.out.println(new _125_Valid_Palindrome().isPalindrome(s));
+		System.out.println(new _125_Valid_Palindrome().isPalindrome2(s));
 
 //		Example 3:
 //		Input: s = " "
@@ -53,14 +63,13 @@ public class _125_Valid_Palindrome {
 //		Since an empty string reads the same forward and backward, it is a palindrome.
 
 		s = " ";
-		System.out.println(new _125_Valid_Palindrome().isPalindrome(s));
-		
-		
+		System.out.println(new _125_Valid_Palindrome().isPalindrome2(s));
+
 		s = ".";
-		System.out.println(new _125_Valid_Palindrome().isPalindrome(s));
-		
+		System.out.println(new _125_Valid_Palindrome().isPalindrome2(s));
+
 		s = "aa";
-		System.out.println(new _125_Valid_Palindrome().isPalindrome(s));
+		System.out.println(new _125_Valid_Palindrome().isPalindrome2(s));
 	}
 
 }
