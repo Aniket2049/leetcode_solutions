e7686812c5e8fd6473f73ce48f9544cf5f527aa2
Aniket Singh <76955108+Aniket2049@users.noreply.github.com>
2024-11-09 21:33:30 +0530

    update print 2d int array utility

--- src/utility/Print.java
+++ src/utility/Print.java
@@ -4,12 +4,20 @@ import java.util.List;
 
 public final class Print {
 	public static void printInt2DArray(int[][] arr) {
+		System.out.print("[");
 		for (int i = 0; i < arr.length; i++) {
+			System.out.print("[");
 			for (int j = 0; j < arr[i].length; j++) {
-				System.out.print(arr[i][j] + " ");
+				if (j == arr[i].length - 1) {
+					System.out.print(arr[i][j]);
+				} else {
+					System.out.print(arr[i][j] + ",");
+				}
 			}
-			System.out.println();
+			System.out.print("]");
 		}
+		System.out.print("]");
+		System.out.println();
 	}
 
 	public static void printStringListOfLists(List<List<String>> listOfList) {
