aeaeb4e6070c314df9c9b6124963f8a547fb9e37
Aniket Singh <76955108+Aniket2049@users.noreply.github.com>
2024-11-18 00:06:11 +0530

    add explanation for 560

--- src/other/_560_Subarray_Sum_Equals_K.java
+++ src/other/_560_Subarray_Sum_Equals_K.java
@@ -37,4 +37,21 @@ public class _560_Subarray_Sum_Equals_K {
 		System.out.println(new _560_Subarray_Sum_Equals_K().subarraySum(new int[] { 1, 2, 3 }, 3));
 	}
 
+//	[a0,  a1,  a2,    a3,   a4,   a5,  a6, ... an]
+//	        ^	                   ^	
+//        sumI	                 sumJ
+//
+//
+//	sumI = sum of numbers till a2 (a0 + a1 + a2)
+//	sumJ = sum of numbers till a5 (a0 + a1 + a2 + a3 + a4 + a5)
+//	
+//	
+//	Now lets say the difference between sumJ and sumI is equal to k. 
+//	What that means is, the sum of numbers between a2 and a5 is equal to k ( a3 + a4 + a5 = k ), which means we found a subarray whose sum is equal to k.
+//	
+//	We can write a3 + a4 + a5 = k as sumJ - sumI = k and sumJ - sumI = k can be written as sumJ - k = sumI
+//	
+//	The expression sumJ - k = sumI, means have we already seen a sum which is equal to sum at current index j minus k. If yes, it means we found a subarray whose sum is equal to k.
+//	
+//	And we keep track of how many times we see a particular sum using a HashMap.
 }
