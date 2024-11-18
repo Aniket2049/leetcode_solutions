213e0cb77820bec389e40a66bde57a0122a89231
Aniket Singh <76955108+Aniket2049@users.noreply.github.com>
2024-11-18 22:58:20 +0530

    add recursive solution to reverse linked list

--- src/tuf/linkedlist1/_206_Reverse_Linked_List.java
+++ src/tuf/linkedlist1/_206_Reverse_Linked_List.java
@@ -2,6 +2,7 @@ package tuf.linkedlist1;
 
 // https://leetcode.com/problems/reverse-linked-list/description/
 // https://www.youtube.com/watch?v=D2vI2DNJGd8
+// https://www.youtube.com/watch?v=G0_I-ZF0S38
 public class _206_Reverse_Linked_List {
 
 	public ListNode reverseList(ListNode head) {
@@ -17,10 +18,41 @@ public class _206_Reverse_Linked_List {
 		return prev;
 	}
 
+	public ListNode reverseListRecursive(ListNode head) {
+        // Base case:
+        // If the linked list is empty or has only one node,
+        // return the head as it is already reversed.
+        if (head == null || head.next == null) {
+            return head;
+        }
+        
+        // Recursive step:
+        // Reverse the linked list starting
+        // from the second node (head.next).
+        ListNode newHead = reverseListRecursive(head.next);
+        
+        // Save a reference to the node following
+        // the current 'head' node.
+        ListNode front = head.next;
+        
+        // Make the 'front' node point to the current
+        // 'head' node in the reversed order.
+        front.next = head;
+        
+        // Break the link from the current 'head' node
+        // to the 'front' node to avoid cycles.
+        head.next = null;
+        
+        // Return the 'newHead,' which is the new
+        // head of the reversed linked list.
+        return newHead;
+    }
+	
 	public static void printList(ListNode head) {
-		while (head != null) {
-			System.out.print(head.val+" ");
-			head = head.next;
+		ListNode temp = head;
+		while (temp != null) {
+			System.out.print(temp.val+" ");
+			temp = temp.next;
 		}
 		System.out.println();
 	}
@@ -43,6 +75,17 @@ public class _206_Reverse_Linked_List {
 //		Example 2:
 //		Input: head = [1,2]
 //		Output: [2,1]
+		node1 = new ListNode(11);
+		node2 = new ListNode(22);
+		node3 = new ListNode(33);
+		node4 = new ListNode(44);
+		node5 = new ListNode(55);
+		node1.next = node2;
+		node2.next = node3;
+		node3.next = node4;
+		node4.next = node5;
+		printList(new _206_Reverse_Linked_List().reverseListRecursive(node1));
+
 
 //		Example 3:
 //		Input: head = []
