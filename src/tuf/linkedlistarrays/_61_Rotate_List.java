0f07aceac567fedd3cbd905d7ed9045c109443bb
Aniket Singh <76955108+Aniket2049@users.noreply.github.com>
2024-11-24 13:57:18 +0530

    add 61 rotate list

---
+++ src/tuf/linkedlistarrays/_61_Rotate_List.java
@@ -0,0 +1,44 @@
+package tuf.linkedlistarrays;
+
+import tuf.linkedlist1.ListNode;
+
+// https://leetcode.com/problems/rotate-list/description/
+// https://www.youtube.com/watch?v=9VPm6nEbVPA
+public class _61_Rotate_List {
+
+	public ListNode rotateRight(ListNode head, int k) {
+		if (head == null || head.next == null || k == 0)
+			return head;
+
+		ListNode current = head;
+		int length = 1;
+		while (current.next != null) {
+			length++;
+			current = current.next;
+		}
+
+		current.next = head;
+		k = length - k % length;
+
+		while (k-- > 0)
+			current = current.next;
+
+		head = current.next;
+		current.next = null;
+
+		return head;
+
+	}
+
+	public static void main(String[] args) {
+//		Example 1:
+//		Input: head = [1,2,3,4,5], k = 2
+//		Output: [4,5,1,2,3]
+
+//		Example 2:
+//		Input: head = [0,1,2], k = 4
+//		Output: [2,0,1]
+
+	}
+
+}
