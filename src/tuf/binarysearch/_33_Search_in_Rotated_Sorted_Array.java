978fa859d9633e0e2c090748baf78587b0d8bfba
Aniket Singh <76955108+Aniket2049@users.noreply.github.com>
2024-12-15 01:23:19 +0530

    add alternative solution to 33 search in rotated sorted array

--- src/tuf/binarysearch/_33_Search_in_Rotated_Sorted_Array.java
+++ src/tuf/binarysearch/_33_Search_in_Rotated_Sorted_Array.java
@@ -54,21 +54,53 @@ public class _33_Search_in_Rotated_Sorted_Array {
 		}
 	}
 
+	public int search2(int[] nums, int target) {
+        int n = nums.length;
+		int low = 0, high = n - 1;
+		while (low <= high) {
+			int mid = (low + high) / 2;
+
+			// if mid points to the target
+			if (nums[mid] == target)
+				return mid;
+
+			// if left part is sorted
+			if (nums[low] <= nums[mid]) {
+				if (nums[low] <= target && target <= nums[mid]) {
+					// element exists
+					high = mid - 1;
+				} else {
+					// element does not exist
+					low = mid + 1;
+				}
+			} else { // if right part is sorted
+				if (nums[mid] <= target && target <= nums[high]) {
+					// element exists
+					low = mid + 1;
+				} else {
+					// element does not exist
+					high = mid - 1;
+				}
+			}
+		}
+		return -1;
+    }
+
 	public static void main(String[] args) {
 //		Example 1:
 //		Input: nums = [4,5,6,7,0,1,2], target = 0
 //		Output: 4
-		System.out.println(new _33_Search_in_Rotated_Sorted_Array().search(new int[] { 4, 5, 6, 7, 0, 1, 2 }, 0));
+		System.out.println(new _33_Search_in_Rotated_Sorted_Array().search2(new int[] { 4, 5, 6, 7, 0, 1, 2 }, 0));
 
 //		Example 2:
 //		Input: nums = [4,5,6,7,0,1,2], target = 3
 //		Output: -1
-		System.out.println(new _33_Search_in_Rotated_Sorted_Array().search(new int[] { 4, 5, 6, 7, 0, 1, 2 }, 3));
+		System.out.println(new _33_Search_in_Rotated_Sorted_Array().search2(new int[] { 4, 5, 6, 7, 0, 1, 2 }, 3));
 
 //		Example 3:
 //		Input: nums = [1], target = 0
 //		Output: -1
-		System.out.println(new _33_Search_in_Rotated_Sorted_Array().search(new int[] { 1 }, 0));
+		System.out.println(new _33_Search_in_Rotated_Sorted_Array().search2(new int[] { 1 }, 0));
 	}
 
 }
