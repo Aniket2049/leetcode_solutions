159733f9900407bf21c652ed94e39dcd11da47fa
Aniket Singh <76955108+Aniket2049@users.noreply.github.com>
2025-01-16 18:36:47 +0530

    add 144 binary tree preorder traversal

---
+++ src/tuf/binarytree/_144_Binary_Tree_Preorder_Traversal.java
@@ -0,0 +1,50 @@
+package tuf.binarytree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// https://leetcode.com/problems/binary-tree-preorder-traversal/description/
+// 
+public class _144_Binary_Tree_Preorder_Traversal {
+
+	List<Integer> answer;
+
+	public class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+
+		TreeNode() {
+		}
+
+		TreeNode(int val) {
+			this.val = val;
+		}
+
+		TreeNode(int val, TreeNode left, TreeNode right) {
+			this.val = val;
+			this.left = left;
+			this.right = right;
+		}
+	}
+
+	public void preorderHelper(TreeNode root, List<Integer> list) {
+		if (root == null)
+			return;
+		preorderHelper(root.left, list);
+		preorderHelper(root.right, list);
+		list.add(root.val);
+	}
+
+	public List<Integer> preorderTraversal(TreeNode root) {
+		List<Integer> answer = new ArrayList<Integer>();
+		preorderHelper(root, answer);
+		return answer;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
