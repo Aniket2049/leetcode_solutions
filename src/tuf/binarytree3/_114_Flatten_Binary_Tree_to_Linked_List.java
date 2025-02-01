0156bae4e04309bec325ec00664ff4c13469fb79
Aniket Singh <76955108+Aniket2049@users.noreply.github.com>
2025-02-01 23:57:05 +0530

    add 114 flatten binary tree to linked list

---
+++ src/tuf/binarytree3/_114_Flatten_Binary_Tree_to_Linked_List.java
@@ -0,0 +1,47 @@
+package tuf.binarytree3;
+
+// https://leetcode.com/problems/flatten-binary-tree-to-linked-list/description/
+// https://leetcode.com/problems/flatten-binary-tree-to-linked-list/solutions/1207642/js-python-java-c-simple-o-1-space-recursive-solutions-w-explanation/
+
+public class _114_Flatten_Binary_Tree_to_Linked_List {
+
+	public void flatten(TreeNode root) {
+		TreeNode curr = root;
+		while (curr != null) {
+			if (curr.left != null) {
+				TreeNode runner = curr.left;
+				while (runner.right != null)
+					runner = runner.right;
+				runner.right = curr.right;
+				curr.right = curr.left;
+				curr.left = null;
+			}
+			curr = curr.right;
+		}
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+
+		TreeNode() {
+		}
+
+		TreeNode(int val) {
+			this.val = val;
+		}
+
+		TreeNode(int val, TreeNode left, TreeNode right) {
+			this.val = val;
+			this.left = left;
+			this.right = right;
+		}
+	}
+
+}
