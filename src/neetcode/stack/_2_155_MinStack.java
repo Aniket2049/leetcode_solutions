a2170e554d6fd8fa63a716764e5569f815391b10
Aniket Singh <76955108+Aniket2049@users.noreply.github.com>
2025-03-09 12:23:07 +0530

    add 155 min stack

---
+++ src/neetcode/stack/_2_155_MinStack.java
@@ -0,0 +1,49 @@
+package neetcode.stack;
+
+import java.util.Stack;
+
+// https://leetcode.com/problems/min-stack/
+// https://www.youtube.com/watch?v=qkLl7nAwDPo
+public class _2_155_MinStack {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
+class MinStack {
+
+	Stack<Integer> stack;
+	Stack<Integer> minStack;
+
+	public MinStack() {
+		stack = new Stack<Integer>();
+		minStack = new Stack<Integer>();
+	}
+
+	public void push(int val) {
+		stack.push(val);
+		int minVal;
+		if (!minStack.isEmpty()) {
+			minVal = minStack.getLast();
+		} else {
+			minVal = val;
+		}
+		minStack.push(Math.min(val, minVal));
+	}
+
+	public void pop() {
+		stack.pop();
+		minStack.pop();
+	}
+
+	public int top() {
+		return stack.peek();
+	}
+
+	public int getMin() {
+		return minStack.peek();
+	}
+}
