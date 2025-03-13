473f7010cfe44502672e71e4fc9f2bf8ae5cc443
Aniket Singh <76955108+Aniket2049@users.noreply.github.com>
2025-03-13 23:37:18 +0530

    add 206 reverse linked list

---
+++ src/neetcode/linkedlist/_1_206_ReverseLinkedList.java
@@ -0,0 +1,56 @@
+package neetcode.linkedlist;
+
+// https://leetcode.com/problems/reverse-linked-list/description/
+// https://www.youtube.com/watch?v=G0_I-ZF0S38
+public class _1_206_ReverseLinkedList {
+
+	public ListNode reverseListIterative(ListNode head) {
+		ListNode prev = null;
+		ListNode curr = head;
+
+		while (curr != null) {
+			ListNode temp = curr.next;
+			curr.next = prev;
+			prev = curr;
+			curr = temp;
+		}
+		return prev;
+	}
+
+	public ListNode reverseListRecursive(ListNode head) {
+		if (head == null) {
+			return null;
+		}
+
+		ListNode newHead = head;
+		if (head.next != null) {
+			newHead = reverseListRecursive(head.next);
+			head.next.next = head;
+		}
+		head.next = null;
+
+		return newHead;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	static class ListNode {
+		int val;
+		ListNode next;
+
+		ListNode() {
+		}
+
+		ListNode(int val) {
+			this.val = val;
+		}
+
+		ListNode(int val, ListNode next) {
+			this.val = val;
+			this.next = next;
+		}
+	}
+}
