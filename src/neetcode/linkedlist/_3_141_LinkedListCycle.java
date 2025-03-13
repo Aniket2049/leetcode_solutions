9c92ee7775ca836c07d926c13b6f32df9df97a1a
Aniket Singh <76955108+Aniket2049@users.noreply.github.com>
2025-03-14 00:13:49 +0530

    add 141 linked list cycle

---
+++ src/neetcode/linkedlist/_3_141_LinkedListCycle.java
@@ -0,0 +1,35 @@
+package neetcode.linkedlist;
+
+// https://leetcode.com/problems/linked-list-cycle/
+// https://www.youtube.com/watch?v=gBTe7lFR3vc
+public class _3_141_LinkedListCycle {
+
+	public boolean hasCycle(ListNode head) {
+		ListNode fast = head;
+		ListNode slow = head;
+
+		while (fast != null && fast.next != null) {
+			fast = fast.next.next;
+			slow = slow.next;
+			if (fast == slow)
+				return true;
+		}
+		return false;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	static class ListNode {
+		int val;
+		ListNode next;
+
+		ListNode(int x) {
+			val = x;
+			next = null;
+		}
+	}
+
+}
