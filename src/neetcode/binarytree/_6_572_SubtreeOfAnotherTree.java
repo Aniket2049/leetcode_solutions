c60f18b63599e27889b8cd895c0c33d8c5154593
Aniket Singh <76955108+Aniket2049@users.noreply.github.com>
2025-03-15 01:07:27 +0530

    add 572 subtree of another tree

---
+++ src/neetcode/binarytree/_6_572_SubtreeOfAnotherTree.java
@@ -0,0 +1,55 @@
+package neetcode.binarytree;
+
+// https://leetcode.com/problems/subtree-of-another-tree/description/
+// 
+public class _6_572_SubtreeOfAnotherTree {
+
+	public boolean isSameTree(TreeNode p, TreeNode q) {
+		if (p == null && q == null) {
+			return true;
+		}
+		if (p != null && q != null && p.val == q.val) {
+			return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+		} else {
+			return false;
+		}
+	}
+
+	public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+		if (subRoot == null) {
+			return true;
+		}
+		if (root == null) {
+			return false;
+		}
+
+		if (isSameTree(root, subRoot)) {
+			return true;
+		}
+		return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	static class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+
+		TreeNode() {
+		}
+
+		TreeNode(int val) {
+			this.val = val;
+		}
+
+		TreeNode(int val, TreeNode left, TreeNode right) {
+			this.val = val;
+			this.left = left;
+			this.right = right;
+		}
+	}
+}
