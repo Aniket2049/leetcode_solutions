ba9fdbf05815cb54a034403485c0e7aed4f1c006
Aniket Singh <76955108+Aniket2049@users.noreply.github.com>
2025-03-19 23:00:15 +0530

    add 261 graph valid tree

---
+++ src/neetcode/graph/_10_261_GraphValidTree.java
@@ -0,0 +1,59 @@
+package neetcode.graph;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+// https://leetcode.com/problems/graph-valid-tree
+// https://neetcode.io/solutions/graph-valid-tree
+// https://www.youtube.com/watch?v=bXsUuownnoQ
+// https://www.lintcode.com/problem/178/
+public class _10_261_GraphValidTree {
+
+	public boolean validTree(int n, int[][] edges) {
+		if (edges.length > n - 1) {
+			return false;
+		}
+
+		List<List<Integer>> adj = new ArrayList<>();
+		for (int i = 0; i < n; i++) {
+			adj.add(new ArrayList<>());
+		}
+
+		for (int[] edge : edges) {
+			adj.get(edge[0]).add(edge[1]);
+			adj.get(edge[1]).add(edge[0]);
+		}
+
+		Set<Integer> visit = new HashSet<>();
+		if (!dfs(0, -1, visit, adj)) {
+			return false;
+		}
+
+		return visit.size() == n;
+	}
+
+	private boolean dfs(int node, int parent, Set<Integer> visit, List<List<Integer>> adj) {
+		if (visit.contains(node)) {
+			return false;
+		}
+
+		visit.add(node);
+		for (int nei : adj.get(node)) {
+			if (nei == parent) {
+				continue;
+			}
+			if (!dfs(nei, node, visit, adj)) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
