057633180a31e3adeb4ddbbeaaf29b2c5a8f7d52
Aniket Singh <76955108+Aniket2049@users.noreply.github.com>
2025-03-21 23:55:19 +0530

    add 70 climbing stairs

---
+++ src/neetcode/dynamic1d/_1_70_ClimbingStairs.java
@@ -0,0 +1,28 @@
+package neetcode.dynamic1d;
+
+// https://leetcode.com/problems/climbing-stairs/
+// https://www.youtube.com/watch?v=UUaMrNOvSqg
+public class _1_70_ClimbingStairs {
+
+	public int climbStairs(int n) {
+
+		if (n == 1)
+			return 1;
+
+		int[] dp = new int[n + 1];
+		dp[1] = 1;
+		dp[2] = 2;
+
+		for (int i = 3; i <= n; i++) {
+			dp[i] = dp[i - 1] + dp[i - 2];
+		}
+
+		return dp[n];
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
