1bee90427ee3bc3db5e1e2486c4a6b63a4e7b6d2
Aniket Singh <76955108+Aniket2049@users.noreply.github.com>
2025-03-22 12:38:43 +0530

    add 746 min cost climbing stairs

---
+++ src/neetcode/dynamic1d/_2_746_MinCostClimbingStairs.java
@@ -0,0 +1,24 @@
+package neetcode.dynamic1d;
+
+// https://leetcode.com/problems/min-cost-climbing-stairs/
+// https://www.youtube.com/watch?v=WeO_E5Q1kGw
+public class _2_746_MinCostClimbingStairs {
+
+	int minCostClimbingStairs(int[] cost) {
+
+		int n = cost.length;
+		int[] minCost = new int[n + 1];
+
+		for (int i = 2; i <= n; i++) {
+			minCost[i] = Math.min((cost[i - 1] + minCost[i - 1]), (cost[i - 2] + minCost[i - 2]));
+		}
+
+		return minCost[n];
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
