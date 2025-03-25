177b92b5b49b4088f33183471965320ca588786a
Aniket Singh <76955108+Aniket2049@users.noreply.github.com>
2025-03-26 01:59:02 +0530

    add another solution to 238 product of array except self

--- src/neetcode/arrays/_7_238_ProductOfArrayExceptSelf.java
+++ src/neetcode/arrays/_7_238_ProductOfArrayExceptSelf.java
@@ -16,7 +16,7 @@ public class _7_238_ProductOfArrayExceptSelf {
 			arr[i] *= right;
 			right *= nums[i];
 		}
-		
+
 		System.out.print("[");
 		for (int i = 0; i < arr.length; i++) {
 			if (i == arr.length - 1)
@@ -25,19 +25,45 @@ public class _7_238_ProductOfArrayExceptSelf {
 				System.out.print(arr[i] + ",");
 		}
 		System.out.print("]\n");
-		
+
 		return arr;
+	}
+
+	public int[] productExceptSelf2(int[] nums) {
+
+		// Array to store all left multiplication
+		int[] left = new int[nums.length];
+
+		// Array to store all right multiplication
+		int[] right = new int[nums.length];
+
+		left[0] = 1;
+		for (int i = 1; i < nums.length; i++) {
+			left[i] = left[i - 1] * nums[i - 1];
+		}
+
+		right[nums.length - 1] = 1;
+		for (int i = nums.length - 2; i > -1; i--) {
+			right[i] = right[i + 1] * nums[i + 1];
+		}
+
+		int[] ans = new int[nums.length];
+		for (int i = 0; i < nums.length; i++) {
+			ans[i] = left[i] * right[i];
+		}
+
+		return ans;
 	}
 
 	public static void main(String[] args) {
 //		Example 1:
 //		Input: nums = [1,2,3,4]
 //		Output: [24,12,8,6]
-		new _7_238_ProductOfArrayExceptSelf().productExceptSelf(new int[] {1,2,3,4});
+		new _7_238_ProductOfArrayExceptSelf().productExceptSelf(new int[] { 1, 2, 3, 4 });
 //		Example 2:
 //		Input: nums = [-1,1,0,-3,3]
 //		Output: [0,0,9,0,0]
-		new _7_238_ProductOfArrayExceptSelf().productExceptSelf(new int[] {-1,1,0,-3,3});
+		new _7_238_ProductOfArrayExceptSelf().productExceptSelf(new int[] { -1, 1, 0, -3, 3 });
 
 	}
 
