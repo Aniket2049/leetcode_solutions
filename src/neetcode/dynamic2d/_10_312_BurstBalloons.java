8dd4886c8e082bf152b9c478fd57ad3effd81246
Aniket Singh <76955108+Aniket2049@users.noreply.github.com>
2025-03-28 00:11:44 +0530

    add another link to 312 burst balloons

--- src/neetcode/dynamic2d/_10_312_BurstBalloons.java
+++ src/neetcode/dynamic2d/_10_312_BurstBalloons.java
@@ -1,9 +1,13 @@
 package neetcode.dynamic2d;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 // https://leetcode.com/problems/burst-balloons/
 // https://www.youtube.com/watch?v=VFskby7lUbw
 // https://algo.monster/liteproblems/312
 // https://www.youtube.com/watch?v=l5AZpRsgcH4
+// https://www.youtube.com/watch?v=Yz4LlDSlkns
 public class _10_312_BurstBalloons {
 	public int maxCoinsBottomUp(int[] nums) {
 		int n = nums.length;
@@ -62,5 +66,41 @@ public class _10_312_BurstBalloons {
 			dp[l][r] = Math.max(dp[l][r], coins);
 		}
 		return dp[l][r];
+	}
+
+	////////////-----TUF
+
+	static int maxCoins(ArrayList<Integer> a) {
+		int n = a.size();
+
+		// Add 1 to the beginning and end of the array
+		a.add(0, 1);
+		a.add(1);
+
+		// Create a 2D DP array
+		int[][] dp = new int[n + 2][n + 2];
+
+		// Iterate from the end to the beginning
+		for (int i = n; i >= 1; i--) {
+			for (int j = 1; j <= n; j++) {
+				if (i > j)
+					continue;
+				int maxi = Integer.MIN_VALUE;
+
+				// Iterate through possible indices to split the array
+				for (int ind = i; ind <= j; ind++) {
+					int cost = a.get(i - 1) * a.get(ind) * a.get(j + 1) + dp[i][ind - 1] + dp[ind + 1][j];
+					maxi = Math.max(maxi, cost);
+				}
+				dp[i][j] = maxi;
+			}
+		}
+		return dp[1][n];
+	}
+
+	public static void main(String[] args) {
+		ArrayList<Integer> a = new ArrayList<>(Arrays.asList(3, 1, 5, 8));
+		int ans = maxCoins(a);
+		System.out.println(ans);
 	}
 }
