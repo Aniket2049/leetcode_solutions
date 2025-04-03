f7f83ab2ae96c2aa50ed1a4357c59579233eddca
Aniket Singh <76955108+Aniket2049@users.noreply.github.com>
2025-04-04 00:00:18 +0530

    add 50 pow x n

---
+++ src/neetcode/math/_6_50_Pow_x_n.java
@@ -0,0 +1,25 @@
+package neetcode.math;
+
+// https://leetcode.com/problems/powx-n/
+// https://www.youtube.com/watch?v=g9YQyYi4IQQ
+public class _6_50_Pow_x_n {
+	public double myPow(double x, int n) {
+		if (x == 0) {
+			return 0;
+		}
+		if (n == 0) {
+			return 1;
+		}
+
+		double res = helper(x, Math.abs((long) n));
+		return (n >= 0) ? res : 1 / res;
+	}
+
+	private double helper(double x, long n) {
+		if (n == 0) {
+			return 1;
+		}
+		double half = helper(x, n / 2);
+		return (n % 2 == 0) ? half * half : x * half * half;
+	}
+}
