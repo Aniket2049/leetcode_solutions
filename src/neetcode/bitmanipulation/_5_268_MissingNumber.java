3b06990a314300b349166664fcb283283b8388b9
Aniket Singh <76955108+Aniket2049@users.noreply.github.com>
2025-04-07 23:40:36 +0530

    add 268 missing number

---
+++ src/neetcode/bitmanipulation/_5_268_MissingNumber.java
@@ -0,0 +1,22 @@
+package neetcode.bitmanipulation;
+
+// https://leetcode.com/problems/missing-number/
+// https://www.youtube.com/watch?v=iCrQG_kbuP0
+public class _5_268_MissingNumber {
+	public int missingNumber(int[] nums) {
+		int allXOR = 0;
+
+		// XOR all numbers in range [0, n]
+		for (int i = 0; i <= nums.length; i++) {
+			allXOR = allXOR ^ i;
+		}
+
+		// XOR all numbers in the given array
+		for (int num : nums) {
+			allXOR = allXOR ^ num;
+		}
+
+		// The missing number
+		return allXOR;
+	}
+}
