490d7e41f0de0158a3b0cbf76594dfc5064d8391
Aniket Singh <76955108+Aniket2049@users.noreply.github.com>
2025-04-07 23:50:04 +0530

    add 371 sum of two integers

---
+++ src/neetcode/bitmanipulation/_6_371_SumOfTwoIntegers.java
@@ -0,0 +1,14 @@
+package neetcode.bitmanipulation;
+
+// https://leetcode.com/problems/sum-of-two-integers/
+// https://www.youtube.com/watch?v=gVUrDV4tZfY
+public class _6_371_SumOfTwoIntegers {
+	public int getSum(int a, int b) {
+		while (b != 0) {
+			int carry = (a & b) << 1;
+			a ^= b;
+			b = carry;
+		}
+		return a;
+	}
+}
