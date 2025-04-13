e911bd1b2596e431f71dfb430f0c1a5695bcfb52
Aniket Singh <76955108+Aniket2049@users.noreply.github.com>
2025-04-13 18:30:21 +0530

    add 88 merge sorted array

---
+++ src/neetcode250/b_twopointers/_5_88_MergeSortedArray.java
@@ -0,0 +1,28 @@
+package neetcode250.b_twopointers;
+
+// https://leetcode.com/problems/merge-sorted-array/
+// https://www.youtube.com/watch?v=P1Ic85RarKY
+public class _5_88_MergeSortedArray {
+	public void merge(int[] nums1, int m, int[] nums2, int n) {
+		int last = m + n - 1;
+
+		// Merge in reverse order
+		while (m > 0 && n > 0) {
+			if (nums1[m - 1] > nums2[n - 1]) {
+				nums1[last] = nums1[m - 1];
+				m--;
+			} else {
+				nums1[last] = nums2[n - 1];
+				n--;
+			}
+			last--;
+		}
+
+		// Fill nums1 with leftover nums2 elements
+		while (n > 0) {
+			nums1[last] = nums2[n - 1];
+			n--;
+			last--;
+		}
+	}
+}
