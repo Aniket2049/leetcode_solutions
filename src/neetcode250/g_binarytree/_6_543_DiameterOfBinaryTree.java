0df0e9d9f71fb8054b924a3354291c7f7ff51e0e
Aniket Singh <76955108+Aniket2049@users.noreply.github.com>
2025-05-05 00:17:19 +0530

    add 543 Diameter of Binary Tree

---
+++ src/neetcode250/g_binarytree/_6_543_DiameterOfBinaryTree.java
@@ -0,0 +1,40 @@
+package neetcode250.g_binarytree;
+
+// https://leetcode.com/problems/diameter-of-binary-tree/
+// https://www.youtube.com/watch?v=K81C31ytOZE
+public class _6_543_DiameterOfBinaryTree {
+	class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+
+		TreeNode() {
+		}
+
+		TreeNode(int val) {
+			this.val = val;
+		}
+
+		TreeNode(int val, TreeNode left, TreeNode right) {
+			this.val = val;
+			this.left = left;
+			this.right = right;
+		}
+	}
+
+	public int diameterOfBinaryTree(TreeNode root) {
+		int[] res = new int[1];
+		dfs(root, res);
+		return res[0];
+	}
+
+	private int dfs(TreeNode root, int[] res) {
+		if (root == null) {
+			return 0;
+		}
+		int left = dfs(root.left, res);
+		int right = dfs(root.right, res);
+		res[0] = Math.max(res[0], left + right);
+		return 1 + Math.max(left, right);
+	}
+}
