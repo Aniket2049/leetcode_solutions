1dcdfa91593c27627b89558aefaf3c8176edbe78
Aniket Singh <76955108+Aniket2049@users.noreply.github.com>
2025-05-07 22:58:22 +0530

    add dfs to 102 Binary Tree Level Order Traversal

--- src/neetcode250/g_binarytree/_13_102_BinaryTreeLevelOrderTraversal.java
+++ src/neetcode250/g_binarytree/_13_102_BinaryTreeLevelOrderTraversal.java
@@ -50,4 +50,25 @@ public class _13_102_BinaryTreeLevelOrderTraversal {
 		}
 		return res;
 	}
+
+	List<List<Integer>> res = new ArrayList<>();
+
+	public List<List<Integer>> levelOrderDFS(TreeNode root) {
+		dfs(root, 0);
+		return res;
+	}
+
+	private void dfs(TreeNode node, int depth) {
+		if (node == null) {
+			return;
+		}
+
+		if (res.size() == depth) {
+			res.add(new ArrayList<>());
+		}
+
+		res.get(depth).add(node.val);
+		dfs(node.left, depth + 1);
+		dfs(node.right, depth + 1);
+	}
 }
