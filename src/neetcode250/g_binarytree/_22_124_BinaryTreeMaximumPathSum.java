33cfe68a6abb07da5c9b5faaab493719d9d61488
Aniket Singh <76955108+Aniket2049@users.noreply.github.com>
2025-05-11 19:28:30 +0530

    add 124 Binary Tree Maximum Path Sum

---
+++ src/neetcode250/g_binarytree/_22_124_BinaryTreeMaximumPathSum.java
@@ -0,0 +1,42 @@
+package neetcode250.g_binarytree;
+
+// https://leetcode.com/problems/binary-tree-maximum-path-sum/
+// https://www.youtube.com/watch?v=Hr5cWUld4vU
+public class _22_124_BinaryTreeMaximumPathSum {
+	class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+
+		TreeNode() {
+		}
+
+		TreeNode(int val) {
+			this.val = val;
+		}
+
+		TreeNode(int val, TreeNode left, TreeNode right) {
+			this.val = val;
+			this.left = left;
+			this.right = right;
+		}
+	}
+
+	public int maxPathSum(TreeNode root) {
+		int[] res = new int[] { root.val };
+		dfs(root, res);
+		return res[0];
+	}
+
+	private int dfs(TreeNode root, int[] res) {
+		if (root == null) {
+			return 0;
+		}
+
+		int leftMax = Math.max(dfs(root.left, res), 0);
+		int rightMax = Math.max(dfs(root.right, res), 0);
+
+		res[0] = Math.max(res[0], root.val + leftMax + rightMax);
+		return root.val + Math.max(leftMax, rightMax);
+	}
+}
