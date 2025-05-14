961d0932b9ff210ff340c6947ca185e369e49fd3
Aniket Singh <76955108+Aniket2049@users.noreply.github.com>
2025-05-15 00:41:17 +0530

    add 767 reorganize string

---
+++ src/neetcode250/h_heap/_8_767_ReorganizeString.java
@@ -0,0 +1,44 @@
+package neetcode250.h_heap;
+
+import java.util.PriorityQueue;
+
+// https://leetcode.com/problems/reorganize-string/
+// https://www.youtube.com/watch?v=2g_b1aYTHeg
+public class _8_767_ReorganizeString {
+	public String reorganizeString(String s) {
+		int[] freq = new int[26];
+		for (char c : s.toCharArray()) {
+			freq[c - 'a']++;
+		}
+
+		PriorityQueue<int[]> maxHeap = new PriorityQueue<>((a, b) -> b[0] - a[0]);
+		for (int i = 0; i < 26; i++) {
+			if (freq[i] > 0) {
+				maxHeap.offer(new int[] { freq[i], i });
+			}
+		}
+
+		StringBuilder res = new StringBuilder();
+		int[] prev = null;
+		while (!maxHeap.isEmpty() || prev != null) {
+			if (prev != null && maxHeap.isEmpty()) {
+				return "";
+			}
+
+			int[] curr = maxHeap.poll();
+			res.append((char) (curr[1] + 'a'));
+			curr[0]--;
+
+			if (prev != null) {
+				maxHeap.offer(prev);
+				prev = null;
+			}
+
+			if (curr[0] > 0) {
+				prev = curr;
+			}
+		}
+
+		return res.toString();
+	}
+}
