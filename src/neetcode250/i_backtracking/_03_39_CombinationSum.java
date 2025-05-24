7522f3cd2d4b200bfe13f51b6cc540f8a9c095d8
Aniket Singh <76955108+Aniket2049@users.noreply.github.com>
2025-05-24 20:20:44 +0530

    add 39 Combination Sum

---
+++ src/neetcode250/i_backtracking/_03_39_CombinationSum.java
@@ -0,0 +1,32 @@
+package neetcode250.i_backtracking;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// https://leetcode.com/problems/combination-sum/
+// https://www.youtube.com/watch?v=GBKI9VSKdGg
+public class _03_39_CombinationSum {
+	List<List<Integer>> res;
+
+	public List<List<Integer>> combinationSum(int[] nums, int target) {
+		res = new ArrayList<List<Integer>>();
+		List<Integer> cur = new ArrayList<Integer>();
+		backtrack(nums, target, cur, 0);
+		return res;
+	}
+
+	public void backtrack(int[] nums, int target, List<Integer> cur, int i) {
+		if (target == 0) {
+			res.add(new ArrayList<Integer>(cur));
+			return;
+		}
+		if (target < 0 || i >= nums.length) {
+			return;
+		}
+
+		cur.add(nums[i]);
+		backtrack(nums, target - nums[i], cur, i);
+		cur.remove(cur.size() - 1);
+		backtrack(nums, target, cur, i + 1);
+	}
+}
