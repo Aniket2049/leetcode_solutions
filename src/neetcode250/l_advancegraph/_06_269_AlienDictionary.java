7b3b36c6adfbbb7cb429766880e7526fd97ac042
Aniket Singh <76955108+Aniket2049@users.noreply.github.com>
2025-06-11 00:22:41 +0530

    add 269 Alien Dictionary

---
+++ src/neetcode250/l_advancegraph/_06_269_AlienDictionary.java
@@ -0,0 +1,71 @@
+package neetcode250.l_advancegraph;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+// https://leetcode.com/problems/alien-dictionary/
+// https://www.youtube.com/watch?v=6kTZYvNNyps
+public class _06_269_AlienDictionary {
+	private Map<Character, Set<Character>> adj;
+	private Map<Character, Boolean> visited;
+	private List<Character> result;
+
+	public String foreignDictionary(String[] words) {
+		adj = new HashMap<>();
+		for (String word : words) {
+			for (char c : word.toCharArray()) {
+				adj.putIfAbsent(c, new HashSet<>());
+			}
+		}
+
+		for (int i = 0; i < words.length - 1; i++) {
+			String w1 = words[i], w2 = words[i + 1];
+			int minLen = Math.min(w1.length(), w2.length());
+			if (w1.length() > w2.length() && w1.substring(0, minLen).equals(w2.substring(0, minLen))) {
+				return "";
+			}
+			for (int j = 0; j < minLen; j++) {
+				if (w1.charAt(j) != w2.charAt(j)) {
+					adj.get(w1.charAt(j)).add(w2.charAt(j));
+					break;
+				}
+			}
+		}
+
+		visited = new HashMap<>();
+		result = new ArrayList<>();
+		for (char c : adj.keySet()) {
+			if (dfs(c)) {
+				return "";
+			}
+		}
+
+		Collections.reverse(result);
+		StringBuilder sb = new StringBuilder();
+		for (char c : result) {
+			sb.append(c);
+		}
+		return sb.toString();
+	}
+
+	private boolean dfs(char ch) {
+		if (visited.containsKey(ch)) {
+			return visited.get(ch);
+		}
+
+		visited.put(ch, true);
+		for (char next : adj.get(ch)) {
+			if (dfs(next)) {
+				return true;
+			}
+		}
+		visited.put(ch, false);
+		result.add(ch);
+		return false;
+	}
+}
