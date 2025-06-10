64bfe85b448b83d2e7b3b5ba35364b2dcd23534f
Aniket Singh <76955108+Aniket2049@users.noreply.github.com>
2025-06-10 23:06:46 +0530

    add 778 Swim in Rising Water

---
+++ src/neetcode250/l_advancegraph/_05_778_SwimInRisingWater.java
@@ -0,0 +1,9 @@
+package neetcode250.l_advancegraph;
+
+// https://leetcode.com/problems/swim-in-rising-water/
+// https://www.youtube.com/watch?v=amvrKlMLuGY
+public class _05_778_SwimInRisingWater {
+    public int swimInWater(int[][] grid) {
+        
+    }
+}
