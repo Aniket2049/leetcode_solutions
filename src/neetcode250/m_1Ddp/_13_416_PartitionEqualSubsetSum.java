1a8c9590140e1cd7cbd1369b44e99260f11f5176
Aniket Singh <76955108+Aniket2049@users.noreply.github.com>
2025-06-21 21:07:38 +0530

    add 416 Partition Equal Subset Sum

---
+++ src/neetcode250/m_1Ddp/_13_416_PartitionEqualSubsetSum.java
@@ -0,0 +1,59 @@
+package neetcode250.m_1Ddp;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+// https://leetcode.com/problems/partition-equal-subset-sum/
+// https://www.youtube.com/watch?v=G5BGAEYGnWU
+public class _13_416_PartitionEqualSubsetSum {
+	int[][] t = new int[201][20001];
+
+	boolean solve(List<Integer> nums, int i, int x) {
+		if (x == 0) {
+			return true;
+		}
+
+		if (i >= nums.size()) {
+			return false;
+		}
+
+		if (t[i][x] != -1) {
+			return t[i][x] == 1;
+		}
+
+		boolean take = false;
+		if (nums.get(i) <= x) {
+			take = solve(nums, i + 1, x - nums.get(i));
+		}
+
+		boolean not_take = solve(nums, i + 1, x);
+
+		t[i][x] = (take || not_take) ? 1 : 0;
+		return take || not_take;
+	}
+
+	public boolean canPartition(int[] numsArr) {
+		List<Integer> nums = new ArrayList<>();
+		for (int num : numsArr)
+			nums.add(num);
+
+		int n = nums.size();
+		int S = 0;
+		for (int num : nums) {
+			S += num;
+		}
+
+		if (S % 2 != 0) {
+			return false;
+		}
+
+		for (int[] row : t) {
+			Arrays.fill(row, -1);
+		}
+
+		int x = S / 2;
+
+		return solve(nums, 0, x);
+	}
+}
