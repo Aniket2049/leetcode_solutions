4cc5e737ada4b3f8eea088caf534ce625c4579b7
Aniket Singh <76955108+Aniket2049@users.noreply.github.com>
2025-06-29 20:27:54 +0530

    update 41 First Missing Positive

--- src/neetcode250/a_arrays/_22_41_FirstMissingPositive.java
+++ src/neetcode250/a_arrays/_22_41_FirstMissingPositive.java
@@ -1,40 +1,32 @@
 package neetcode250.a_arrays;
 
 // https://leetcode.com/problems/first-missing-positive/
-// https://www.youtube.com/watch?v=lyjOwaUEWWQ
+// https://www.youtube.com/watch?v=8g78yfzMlao
 public class _22_41_FirstMissingPositive {
 	public int firstMissingPositive(int[] nums) {
 		int n = nums.length;
-		boolean contains1 = false;
 
 		for (int i = 0; i < n; i++) {
-			// Check whether 1 is in the original array
-			if (nums[i] == 1) {
-				contains1 = true;
-			}
-			if (nums[i] <= 0 || nums[i] > n) {
-				nums[i] = 1;
+			if (nums[i] < 0) {
+				nums[i] = 0;
 			}
 		}
 
-		if (!contains1)
-			return 1;
-
-		// Mark whether integers 1 to n are in nums
-		// Use index as a hash key and negative sign as a presence detector.
 		for (int i = 0; i < n; i++) {
 			int val = Math.abs(nums[i]);
-			int idx = val - 1;
-
-			if (nums[idx] < 0)
-				continue;
-			nums[idx] *= -1;
+			if (val >= 1 && val <= n) {
+				if (nums[val - 1] > 0) {
+					nums[val - 1] *= -1;
+				} else if (nums[val - 1] == 0) {
+					nums[val - 1] = -1 * (n + 1);
+				}
+			}
 		}
 
-		// First positive in nums is smallest missing positive integer
-		for (int i = 0; i < n; i++) {
-			if (nums[i] > 0)
-				return i + 1;
+		for (int i = 1; i <= n; i++) {
+			if (nums[i - 1] >= 0) {
+				return i;
+			}
 		}
 
 		return n + 1;
