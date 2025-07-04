b0d93377c4886b47d09cbdfe2340a07f23f77990
Aniket Singh <76955108+Aniket2049@users.noreply.github.com>
2025-07-04 16:54:28 +0530

    add 134 Gas Station

---
+++ src/neetcode250/o_greedy/_08_134_GasStation.java
@@ -0,0 +1,35 @@
+package neetcode250.o_greedy;
+
+// https://leetcode.com/problems/gas-station/
+// https://www.youtube.com/watch?v=fOaUh1_fJPw
+public class _08_134_GasStation {
+	public int canCompleteCircuit(int[] gas, int[] cost) {
+		int totalGas = 0, totalCost = 0;
+
+		// Calculate total gas and total cost
+		for (int i = 0; i < gas.length; i++) {
+			totalGas += gas[i];
+			totalCost += cost[i];
+		}
+
+		// If total gas is less than total cost, return -1
+		if (totalGas < totalCost) {
+			return -1;
+		}
+
+		int currentGas = 0, startIndex = 0;
+		// Iterate through the gas stations
+		for (int i = 0; i < gas.length; i++) {
+			currentGas += gas[i] - cost[i];
+
+			// If current gas is negative, reset start index and current gas
+			if (currentGas < 0) {
+				startIndex = i + 1;
+				currentGas = 0;
+			}
+		}
+
+		// Return the starting index if a valid circuit exists
+		return startIndex;
+	}
+}
