3df80ce21b0be160b1c7cd196ca31318a5ffbe84
Aniket Singh <76955108+Aniket2049@users.noreply.github.com>
2025-07-06 23:44:27 +0530

    add 135 Candy

---
+++ src/neetcode250/o_greedy/_14_135_Candy.java
@@ -0,0 +1,9 @@
+package neetcode250.o_greedy;
+
+// https://leetcode.com/problems/candy/
+// https://www.youtube.com/watch?v=1IzCRCcK17A
+public class _14_135_Candy {
+    public int candy(int[] ratings) {
+        
+    }
+}
