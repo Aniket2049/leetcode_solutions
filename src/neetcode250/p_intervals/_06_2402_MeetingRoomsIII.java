5cd7056323c374d914441100f6b4e9769804f157
Aniket Singh <76955108+Aniket2049@users.noreply.github.com>
2025-07-11 00:49:16 +0530

    add 2402 Meeting Rooms III

---
+++ src/neetcode250/p_intervals/_06_2402_MeetingRoomsIII.java
@@ -0,0 +1,46 @@
+package neetcode250.p_intervals;
+
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
+// https://leetcode.com/problems/meeting-rooms-iii/
+// https://www.youtube.com/watch?v=2VLwjvODQbA
+public class _06_2402_MeetingRoomsIII {
+	public int mostBooked(int n, int[][] meetings) {
+		Arrays.sort(meetings, (a, b) -> Long.compare(a[0], b[0]));
+		PriorityQueue<Integer> available = new PriorityQueue<>();
+		PriorityQueue<long[]> used = new PriorityQueue<>(
+				(a, b) -> a[0] == b[0] ? Long.compare(a[1], b[1]) : Long.compare(a[0], b[0]));
+		for (int i = 0; i < n; i++) {
+			available.offer(i);
+		}
+		int[] count = new int[n];
+
+		for (int[] meeting : meetings) {
+			long start = meeting[0];
+			long end = meeting[1];
+			while (!used.isEmpty() && used.peek()[0] <= start) {
+				int room = (int) used.poll()[1];
+				available.offer(room);
+			}
+			if (available.isEmpty()) {
+				long[] current = used.poll();
+				int room = (int) current[1];
+				end = current[0] + (end - start);
+				available.offer(room);
+			}
+
+			int room = available.poll();
+			used.offer(new long[] { end, room });
+			count[room]++;
+		}
+
+		int maxRoom = 0;
+		for (int i = 1; i < n; i++) {
+			if (count[i] > count[maxRoom]) {
+				maxRoom = i;
+			}
+		}
+		return maxRoom;
+	}
+}
