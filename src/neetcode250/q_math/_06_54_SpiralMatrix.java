c841389b731b5f70e461fad1b055fcbafc80cc5f
Aniket Singh <76955108+Aniket2049@users.noreply.github.com>
2025-07-16 01:02:44 +0530

    add 54 Spiral Matrix

---
+++ src/neetcode250/q_math/_06_54_SpiralMatrix.java
@@ -0,0 +1,9 @@
+package neetcode250.q_math;
+
+// https://leetcode.com/problems/spiral-matrix/
+// https://www.youtube.com/watch?v=aqVW8IuXUF0
+public class _06_54_SpiralMatrix {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        
+    }
+}
