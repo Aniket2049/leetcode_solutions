decf3ed1c34b0462504e3c4454b3d5d3cf4b80aa
Aniket Singh <76955108+Aniket2049@users.noreply.github.com>
2025-07-22 23:22:10 +0530

    add 338 Counting Bits

---
+++ src/neetcode250/r_bitmanipulation/_03_338_CountingBits.java
@@ -0,0 +1,18 @@
+package neetcode250.r_bitmanipulation;
+
+// https://leetcode.com/problems/counting-bits/
+// https://www.youtube.com/watch?v=RyBM56RIWrM
+public class _03_338_CountingBits {
+	public int[] countBits(int n) {
+		int[] dp = new int[n + 1];
+		int offset = 1;
+
+		for (int i = 1; i <= n; i++) {
+			if (offset * 2 == i) {
+				offset = i;
+			}
+			dp[i] = 1 + dp[i - offset];
+		}
+		return dp;
+	}
+}
