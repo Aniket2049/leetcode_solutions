9c9f371378614d8a3dee38781d292dc23bcdd497
Aniket Singh <76955108+Aniket2049@users.noreply.github.com>
2025-07-22 23:13:02 +0530

    add 191 Number of 1 Bits

---
+++ src/neetcode250/r_bitmanipulation/_02_136_SingleNumber.java
@@ -0,0 +1,14 @@
+package neetcode250.r_bitmanipulation;
+
+// https://leetcode.com/problems/number-of-1-bits/
+// https://www.youtube.com/watch?v=5Km3utixwZs
+public class _02_136_SingleNumber {
+	public int hammingWeight(int n) {
+		int res = 0;
+		while (n != 0) {
+			res += (n & 1) == 1 ? 1 : 0;
+			n >>= 1;
+		}
+		return res;
+	}
+}
