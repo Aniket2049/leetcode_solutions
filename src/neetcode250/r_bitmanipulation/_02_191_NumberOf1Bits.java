9bcbd24d08fa94f97f958fa25742a4af829e2d60
Aniket Singh <76955108+Aniket2049@users.noreply.github.com>
2025-07-23 23:39:32 +0530

    fix filename 191 Number of 1 Bits

--- src/neetcode250/r_bitmanipulation/_02_191_NumberOf1Bits.java
+++ src/neetcode250/r_bitmanipulation/_02_191_NumberOf1Bits.java
@@ -2,7 +2,7 @@ package neetcode250.r_bitmanipulation;
 
 // https://leetcode.com/problems/number-of-1-bits/
 // https://www.youtube.com/watch?v=5Km3utixwZs
-public class _02_136_SingleNumber {
+public class _02_191_NumberOf1Bits {
 	public int hammingWeight(int n) {
 		int res = 0;
 		while (n != 0) {
