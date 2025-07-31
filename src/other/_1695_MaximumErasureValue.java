e749a9c5f531da7cb697991c2171cd0a670971f3
Aniket Singh <76955108+Aniket2049@users.noreply.github.com>
2025-07-31 23:03:52 +0530

    add 1695 Maximum Erasure Value

---
+++ src/other/_1695_MaximumErasureValue.java
@@ -0,0 +1,25 @@
+package other;
+
+import java.util.HashSet;
+
+// https://leetcode.com/problems/maximum-erasure-value
+public class _1695_MaximumErasureValue {
+	public int maximumUniqueSubarray(int[] nums) {
+		HashSet<Integer> set = new HashSet<>();
+		int maxSum = 0, currSum = 0;
+		int left = 0;
+
+		for (int right = 0; right < nums.length; right++) {
+			while (set.contains(nums[right])) {
+				set.remove(nums[left]);
+				currSum -= nums[left];
+				left++;
+			}
+			set.add(nums[right]);
+			currSum += nums[right];
+			maxSum = Math.max(maxSum, currSum);
+		}
+
+		return maxSum;
+	}
+}
